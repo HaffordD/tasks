9b144fb7e15dbbdc9b4ebd786997a344b32a76d7
HaffordD <drew.hafford@maine.edu>
2026-02-03 17:43:43 -0500

    did the nightmare of a check for ?

--- src/functions.ts
+++ src/functions.ts
@@ -42,7 +42,10 @@ export function shout(message: string): string {
  * mark. Do not use an `if` statement in solving this question.
  */
 export function isQuestion(message: string): boolean {
-    return true;
+    let check:boolean = false;
+    let len:number = message.length;
+    check = (message.includes("?", len-1) ?true:false)
+    return check;
 }
 
 /**

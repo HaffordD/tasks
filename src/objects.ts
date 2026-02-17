a61c7fe6d1762bad5977f7a21a0e310944117ed1
HaffordD <drew.hafford@maine.edu>
2026-02-17 18:35:55 -0500

    did number 7

--- src/objects.ts
+++ src/objects.ts
@@ -128,7 +128,11 @@ export function renameQuestion(question: Question, newName: string): Question {
  * published; if it was published, now it should be not published.
  */
 export function publishQuestion(question: Question): Question {
-    return question;
+    if(question.published === true)
+        return({...question,published:false})
+    else{
+        return({...question, published:true})
+    }
 }
 
 /**

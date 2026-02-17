d6cbe61aa4273c2d18eb9f138f8f3263ae4dc2f3
HaffordD <drew.hafford@maine.edu>
2026-02-17 18:51:33 -0500

    did 9

--- src/objects.ts
+++ src/objects.ts
@@ -154,7 +154,8 @@ export function duplicateQuestion(id: number, oldQuestion: Question): Question {
  * Check out the subsection about "Nested Fields" for more information.
  */
 export function addOption(question: Question, newOption: string): Question {
-    return question;
+    const newQuest = {...question, options: [...question.options, newOption]};
+    return newQuest;
 }
 
 /**
